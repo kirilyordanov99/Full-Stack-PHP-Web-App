b26d9f1c830b00704979b1c32138276e57444537
Kiril Yordanov <32341596+kirilyordanov99@users.noreply.github.com>
2021-10-12 13:00:43 +0300

    Added database

---
+++ database/factories/BrandModelFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\BrandModel::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2019_01_12_020529_create_cars_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('brand_model_id');
+            $table->unsignedInteger('brand_id');
+            $table->year('year_of_production');
+            $table->unsignedInteger('mileage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
